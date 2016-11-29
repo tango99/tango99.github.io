939d25c22c102714d771948c27ea25b7d00205db
xiTango <lmccauley1999@gmail.com>
2016-11-29 20:04:20 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,11 +2,11 @@ function styleReef() {
     document.getElementById("welcome").style.color = "#3A7BD5";
     document.getElementsByClassName("page-header")[0].style.background = "linear-gradient(120deg, #00D2FF, #3A7BD5)"; 
 }
-function stlyeFire() {
+function styleFire() {
     document.getElementById("welcome").style.color = "#C02425";
     document.getElementsByClassName("page-head")[0].style.background = "linear-gradient(120deg, #C02425, #F0CB35)";
 }
-function stlyeLush() {
+function styleLush() {
     document.getElementById("welcome").style.color = "#A8E063";
     document.getElementsByClassName("page-head")[0].style.background = "linear-gradient(120deg, #56AB2F, #A8E063)";
 }
