95c68366f2aa4a01086712cbf471a7996b07a5c5
xiTango <lmccauley1999@gmail.com>
2016-11-29 19:32:38 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 function styleReef() {
-    document.getElementById("welcome").style.color = "blue";
+    document.getElementById("welcome").style.color = "#3A7BD5";
     document.getElementsByClassName("page-header")[0].style.background = "linear-gradient(120deg, #00D2FF, #3A7BD5)";
 }
